2388a611b39243be6e008625f46eca71a9abe4dd
Sophia G <137671339+sopgorma@users.noreply.github.com>
2023-11-30 01:41:18 -0500

    Update GormanSophiaM06_FinalProject.py

--- GormanSophiaM06_FinalProject.py
+++ GormanSophiaM06_FinalProject.py
@@ -10,8 +10,8 @@ Final Project - M06
 pity = str(input("Did you previously 'win' or 'lose' your pity? (If you are unsure, put 'win'.) "))
 chance = int(input("Enter the amount of wishes made after obtaining last 6-star character: "))
 #Every 75-90 wishes, the user will be put into a "pity system"
-#This pity system will give the user a 50/50 chance of either "winning" a banner character, 
-#or losing, and obtaining a regular 6-star character.
+#This pity system will give the user a 50/50 chance of either "winning" a 5-star banner character, 
+#or losing, and obtaining a regular 5-star character.
 
 if pity == "lose":
     if chance >= 75:
@@ -22,11 +22,11 @@ if pity == "lose":
     
 if pity == "win":
     if chance >= 75:
-        print("You will get a regular 6-star character within the next 15 wishes. You still need another 75-90 wishes to obtain the banner character.")
+        print("You will get a regular 5-star character within the next 15 wishes. You still need another 75-90 wishes to obtain the banner character.")
     elif chance < 75:
         chance = 75 - chance
-        print("You still need ", chance, " wishes to get into the pity draw to obtain a 6-star character. Afterwards, it will take you another 75-90 wishes to obtain the banner character. Good luck!")
+        print("You still need ", chance, " wishes to get into the pity draw to obtain a 5-star character. Afterwards, it will take you another 75-90 wishes to obtain the banner character. Good luck!")
     else:
         print("Invalid. Please try again")
 
-#These functions tell the user how many more wishes they need to obtain a 6-star character, whether banner or regular
+#These functions tell the user how many more wishes they need to obtain a 5-star character, whether banner or regular
