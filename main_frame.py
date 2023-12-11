fcfeeeaa17649e49b8dc8ae9e09a5ae28466ce41
Sophia G <137671339+sopgorma@users.noreply.github.com>
2023-12-11 11:38:56 -0500

    Delete main_frame.py

--- main_frame.py
+++
@@ -1,6 +0,0 @@
-from breezypythongui import EasyFrame
-
-class MainFrame(EasyFrame):
-    def __init__(self):
-        EasyFrame.__init__(self, title="~Genshin Pity~")
-        self.config(bg="darksalmon")
