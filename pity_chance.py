68ce807db362263855257e306f036a43863b32be
Sophia G <137671339+sopgorma@users.noreply.github.com>
2023-12-11 11:39:40 -0500

    Update pity_chance.py

--- pity_chance.py
+++ pity_chance.py
@@ -4,52 +4,47 @@ class MainFrame(EasyFrame):
     def __init__(self):
         EasyFrame.__init__(self, title="~Genshin Pity~")
         self.config(bg="darksalmon")
-        self.won = 0
-        self.lose = 0
 
-        self.addButton(text="Won", row=1, column=2, command=self.won)
-        self.addButton(text="Lost", row=1, column=4, command=self.lose)
+class Pity():
+    def __init__(self, pity, chance):
+        self.pity = input("Did you previously 'win' or 'lose' your pity? (If you are unsure, put 'win'.) ")
+        self.chance = int(input("Enter the amount of wishes made after obtaining last 5-star character: "))
 
-    def primoCost():
-        
+    def get_pity(self):
+        return self.pity
+    
+    def get_chance(self):
+        return self.chance
 
+    def lose(self):
+        if self.pity == "lose":
+            if self.chance >= 75:
+                self.chance = 90 - self.chance
+                chanceCost = float((self.chance/15)*primoCost)
+                print("You will get the banner character within the next ",self.chance," wish(es)  or $", f'{chanceCost:.2f}',". Congratulations!")
+            elif self.chance < 75:
+                self.chance = 75 - self.chance
+                chanceCost = float((self.chance/15)*primoCost)
+                totalCost = chanceCost + primoCost
+                print("You still need ",self.chance," wishes or roughly $",f'{chanceCost:.2f}'," to get into the pity draw.," '\n', "After that, you will obtain the banner character within 15 wishes. You need to spend a max total of $", f'{totalCost:.2f}', ". You got this!")
+    
+    def win(self):
+        if self.pity == "win":
+            if self.chance >= 75:
+                self.chance = 90 - self.chance
+                chanceCost = chanceCost = float((self.chance/15)*primoCost)
+                print ("You're still in the pity! You need ", self.chance, " wishes or  $",f'{chanceCost:.2f}'," to see if you win.", '\n', "If you lose, it will cost around $", cost, "to be guaranteed the next banner character.")
+            elif self.chance < 75:
+                self.chance = 75 - self.chance
+                chanceCost = float((self.chance/15)*primoCost)
+                totalCost = chanceCost + primoCost
+                print ("You still need ",self.chance," wishes or roughly $", f'{chanceCost:.2f}', "to begin the soft pity. You will need a max total of $", f'{totalCost:.2f}', "to obtain a 5-star character.", '\n', "You are still in the pity so you have a 50/50 chance to obtain the banner character.", '\n', "If you lose, it will cost around $", cost, "to be guaranteed the next banner character. Good luck!")
 
-primogems = 160 #160 primogems = 1 wish. 1600 primos = 10 wishes. 
-primoCost = 35.00 #It costs roughly $35.00 for 15 wishes/pulls. (~$23 for 10 pulls)
-costs = 200.00 #It costs at least $200.00 for 90 pulls
-#Players have the option to wish one time, or pull 10 wishes at once. 
-print("Welcome to the Genshin Pity System program! If you previously won your 50/50 pity, you will still be in the pity on your next 75-90 wishes. ", '\n', "If you lost, you are guaranteed the banner character in the next 90 wishes. Good luck and have fun! :D")
+primogems = 160 
+primoCost = 35.00 
+cost = 200.00 
 
-pity = str(input("Did you previously 'win' or 'lose' your pity? (If you are unsure, put 'win'.) "))
-chance = int(input("Enter the amount of wishes made after obtaining last 5-star character: "))
-#Every 75-90 wishes, the user will be put into a "pity system".
-#This pity system will give the user a 50/50 chance of either "winning" a banner character, 
-#or losing, and obtaining a regular 5-star character. If a character wins, they stay in the pity for the next 90 wishes.
-#If they lose, their next banner character is guaranteed.
-def lose():
-    if pity == "lose":
-        if chance >= 75:
-            chance = 90 - chance
-            chanceCost = float((chance/15)*primoCost)
-            print("You will get the banner character within the next ",chance," wish(es)  or $", chanceCost,". Congratulations!")
-        elif chance < 75:
-            chance = 75 - chance
-            chanceCost = float((chance/15)*primoCost)
-            totalCost = chanceCost + primoCost
-            print("You still need ",chance," wishes or roughly $",chanceCost," to get into the pity draw.," '\n', "After that, you will obtain the banner character within 15 wishes. You need to spend a max total of $", totalCost, ". You got this!")
-
-def win():
-    if pity == "win":
-        if chance >= 75:
-            chance = 90 - chance
-            chanceCost = chanceCost = float((chance/15)*primoCost)
-            print("You're still in the pity! You need ", chance, " wishes or  $",chanceCost," to see if you win.")
-            print("If you lose, it will cost around $", costs, "to be guaranteed the next banner character.")
-        elif chance < 75:
-            chance = 75 - chance
-            chanceCost = float((chance/15)*primoCost)
-            totalCost = chanceCost + primoCost
-            print("You still need ",chance," wishes or roughly $", chanceCost, "to begin the soft pity. You will need a max total of $", totalCost, "to obtain a 5-star character.")
-            print("You are still in the pity so you have a 50/50 chance to obtain the banner character.", '\n', "If you lose, it will cost around $", costs, "to be guaranteed the next banner character. Good luck!")
-
-#These functions tell the user how many more wishes they need to obtain a 5-star character, whether banner or regular
+totalPity = Pity(any,any)
+print(totalPity)
+totalPity.lose()
+totalPity.win()
