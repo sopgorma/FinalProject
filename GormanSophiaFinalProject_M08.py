81877ace53186c0827e2f3175edd415654d6214b
Sophia G <137671339+sopgorma@users.noreply.github.com>
2023-12-15 14:21:57 -0500

    Update GormanSophiaFinalProject_M08.py

--- GormanSophiaFinalProject_M08.py
+++ GormanSophiaFinalProject_M08.py
@@ -7,31 +7,32 @@ Final Project - Genshin Pity
 #This program will determine how many pulls you need to get the wanted "banner" character.
 #Based on the popular game "Genshin Impact". This program is based on purchasing primogems instead of farming them.
 
-from breezypythongui import EasyFrame
-from tkinter import PhotoImage
-import tkinter as tk
-from main_frame import MainFrame
-from pity_chance import 
+#This is the main frame used to run the program. Have fun!
 
+from breezypythongui import EasyFrame
+import tkinter as tk
+from picture_frame import IMGUI
+from pity_chance import Pity
+
+#Create a window and put the images from IMGUI into frame
 window = tk.Tk()
 
-frame = MainFrame()
-frame.grid(row=0, column=0)
+frame = IMGUI()
+frame.grid(row=0,column=0)
 
-frame = None
-
+#This will begin the application, with an "End Session" button if wanting to quit
 def start_pity():
     global frame
-    pity_chance = Pity()
-    pity_chance.grid(row=0, column= 0, sticky= "NSEW")
-    calculate = tk.Button(pity_chance, text="Calculate Total", command=calculate_pity)
-    calculate.grid(row=1, column=1)
-
-def calculate_pity():
-    frame.grid_forget()
+    frame = Pity(input)
     frame.grid(row=0, column= 0, sticky= "NSEW")
+    end_s = tk.Button(frame, text="End Session", command=end_session)
+    end_s.grid(row=4, column=0)
 
-begin = tk.Button(frame, text="Will you get the banner character?", command=start_pity)
-begin.grid(row=0, column=0)
+def end_session():
+    frame.grid_forget()
+
+begin = tk.Button(frame, text="Let's begin!", width=15, height=3,command=start_pity, bg="chartreuse2", fg="black", font = "bold")
+begin.grid(row=1, column=0)
+
 
 window.mainloop()
